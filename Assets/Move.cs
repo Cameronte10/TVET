8d9eafbb53eb58ac8f7e0b86c433ec5abb3af39d
Cameronte10 <cjbaker30@yahoo.com.au>
2018-02-20 17:35:51 +1100

    Dropped my Balls and made them GLOW

---
+++ Assets/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float thrust;
+    public Rigidbody rb;
+    public Transform tr;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey((KeyCode.W)))
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+            transform.position = new Vector3(0, 0, 0);
+        }
+    
+}
