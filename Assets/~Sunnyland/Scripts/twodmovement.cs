a1304f1b3460fc26483abf768ed78ad02830c7c0
Cameronte10 <cjbaker30@yahoo.com.au>
2018-03-27 18:01:23 +1100

    Added tmx tsx

---
+++ Assets/~Sunnyland/Scripts/twodmovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twodmovement : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust);
+    }
+}
