756d059add2109c9f770697d5ddfdd6a9ed38eb0
Cameronte10 <cjbaker30@yahoo.com.au>
2018-06-04 20:26:50 +1000

    added stiff for assig
    sgdfsg

---
+++ Assets/~TopDownShooter/Scripts/AI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+
+    public Transform target;
+	public float speed;
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
---
+++ Assets/~TopDownShooter/Scripts/pew.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pew : MonoBehaviour {
+    public float thrust;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.forward * thrust);
+    }
+}
