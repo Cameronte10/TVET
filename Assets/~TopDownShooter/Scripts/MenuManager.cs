ce877a217ac23651f77a685f075d9a2b606735c5
Cameronte10 <cjbaker30@yahoo.com.au>
2018-06-11 20:50:16 +1000

    FINSIHED
    almost

---
+++ Assets/~TopDownShooter/Scripts/MenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+   
+    public bool gameScene;
+    public GameObject canvas;
+    public float timer;
+
+    void Start()
+    {
+        //controller = GameObject.FindObjectWithTag("Player").GetComponent<FirstPersonController>();
+        canvas = GameObject.Find("Canvas");
+        if (gameScene)
+        {
+            canvas.GetComponent<Canvas>().enabled = false;
+        }
+    }
+    public void PLayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape) && gameScene)
+        {
+            
+            if (canvas.GetComponent<Canvas>().enabled == true)
+            {
+                //controller.enabled = true;
+                //Cursor.lockState = CursorLockMode.Locked;
+                //Cursor.visible = false
+                Time.timeScale = 1;
+                canvas.GetComponent<Canvas>().enabled = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                canvas.GetComponent<Canvas>().enabled = true;
+            }
+        }
+    }
+
+}
---
+++ Assets/~TopDownShooter/Scripts/ShootRay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShootRay : MonoBehaviour
+{
+    public float distance = 30f;
+    public int kills = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hitInfo;
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hitInfo, distance))
+            {
+                Debug.DrawLine(ray.origin, ray.GetPoint(30f), Color.blue);
+                if (hitInfo.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Hit Enemy");
+                    Destroy(hitInfo.transform.gameObject);
+                    kills++;
+                    
+                }
+            }
+        }
+        if(kills >= 30)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+    
+}
